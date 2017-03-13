da56e21f4c7d6f4d91ccf9bf442931fe101540a9
James Joyce <pjamesjoyce@gmail.com>
2017-03-13 10:21:05 +0100

    remove old django interact implementation

--- lcopt_interactive/lcopt_interactive_root/lcopt_create/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LcoptCreateConfig(AppConfig):
-    name = 'lcopt_create'
--- lcopt_interactive/lcopt_interactive_root/sandbox/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from sandbox.models import SandboxPositions
-# Register your models here.
-
-admin.site.register(SandboxPositions)
